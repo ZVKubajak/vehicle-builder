ad56694a6744e3d88162fe120d4310939accbdbd
VanceGoblin <zvkubajak@gmail.com>
2024-08-01 08:58:54 -0500

    Created Code for Tow and Wheelie Methods

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -39,7 +39,11 @@ class Motorbike extends Vehicle {
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
   wheelie(): void {
-    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    if (this instanceof Motorbike) {
+      console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    } else {
+      console.log('Cars and trucks cannot do wheelies.');
+    }
   }
 
   // TODO: Override the printDetails method from the Vehicle class
--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -47,6 +47,16 @@ class Truck extends Vehicle implements AbleToTow {
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
     // TODO: If it is, log that the vehicle is being towed
     // TODO: If it is not, log that the vehicle is too heavy to be towed
+    if (vehicle.make && vehicle.model) {
+      const vehicleMake = vehicle.make;
+      const vehicleModel = vehicle.model;
+
+      if (vehicle.weight <= this.towingCapacity) {
+        console.log(`Towing ${vehicleMake} ${vehicleModel}.`);
+      } else {
+        console.log(`The ${vehicleMake} ${vehicleModel} is too heavy to be towed.`);
+      }
+    }
   }
 
   // TODO: Override the printDetails method from the Vehicle class
